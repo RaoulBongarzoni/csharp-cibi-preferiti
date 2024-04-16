40a8b947bc89c30d0b8b0bae382f32423d5b6ced
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2024-04-16 16:36:38 +0200

    Exercise Solution

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace csharp_cibi_preferiti
+﻿namespace CibiPreferiti
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //inizializzo l'array
+            string[] CiboArray = { "pasta", "pizza", "gelato", "lasagna", "cioccolato", "caffe" };
+
+            //stampo a schermo la lunghezza
+            Console.WriteLine($"l'array è lungo {CiboArray.Length}");
+
+            //stampo ogni elemento preceduto dalla sua posizione
+            var i = 1;
+            foreach (string Cibo in CiboArray)
+            {
+                Console.Write($" {i} - ");
+                Console.WriteLine(Cibo);
+                i++;
+            }
+
+            //stampo il primo elemento
+            Console.WriteLine(CiboArray[0]);
+
+            //e stampo l'ultimo elemento
+            Console.WriteLine(CiboArray[CiboArray.Length - 1]);
+
+            //e anche l'elemento in mezzo
+            Console.WriteLine(CiboArray[CiboArray.Length / 2]);
         }
     }
 }
